b1761b94f066eee7c0fb7ffe39c99d16a160d983
Russell Goldenberg <russellgoldenberg@gmail.com>
2022-03-23 12:08:11 -0400

    Initial commit

---
+++ src/routes/index.svelte
@@ -0,0 +1,9 @@
+<script>
+  import { browser } from "$app/env";
+  import Meta from "$components/Meta.svelte";
+  import App from "$components/App.svelte";
+  if (browser) console.log("svelte-starter version", __VERSION__);
+</script>
+
+<Meta />
+<App />
