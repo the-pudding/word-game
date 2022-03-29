94c17575515bf2bd1c83b3a6d6125affb56bb235
russellgoldenberg <russellgoldenberg@gmail.com>
2022-03-29 11:58:24 -0400

    fixes #1

--- src/components/Modal.svelte
+++ src/components/Modal.svelte
@@ -1,9 +1,20 @@
 <script>
+  import { tweened } from "svelte/motion";
   import { active, round } from "$stores/misc.js";
 
   export let buttonText = "";
 
-  const onBegin = () => {
+  const start = 3;
+  const end = -1;
+  const duration = start * 1000;
+
+  const countdown = tweened(start, { duration });
+  $: visible = $countdown < start;
+  $: if ($countdown <= end) startRound();
+  $: timer = $countdown < 0 ? "Begin!" : Math.floor($countdown) + 1;
+
+  const onBegin = () => countdown.set(end);
+  const startRound = () => {
     $active = true;
     $round += 1;
   };
@@ -11,3 +22,16 @@
 
 <slot />
 <button on:click={onBegin}>{buttonText}</button>
+<p class:visible>{timer}</p>
+
+<style>
+  p {
+    display: none;
+    font-size: 4em;
+    padding: 2em;
+  }
+
+  .visible {
+    display: block;
+  }
+</style>
