31c8d74507dac0a9743ea8240177600fe7035d15
russellgoldenberg <russellgoldenberg@gmail.com>
2022-07-22 10:39:04 -0400

    chunk component

---
+++ src/components/Chunk.svelte
@@ -0,0 +1,15 @@
+<script>
+	export let text = "";
+	export let max = 20;
+
+	$: regex = new RegExp(`[\\s\\S]{1,${max}}(?!\\S)`, "g");
+	$: chunks = text
+		.replace(regex, "$&\n")
+		.split("\n")
+		.filter((d) => d)
+		.map((d, i) => `${i > 0 ? " " : ""}${d}`);
+</script>
+
+{#each chunks as text}
+	<span>{text}</span>
+{/each}
