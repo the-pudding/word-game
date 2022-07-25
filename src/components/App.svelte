bf9f7bc10b8b889981a845664a1b5b3b36cc0052
russellgoldenberg <russellgoldenberg@gmail.com>
2022-07-25 08:40:29 -0400

    chunk and colors

--- src/components/App.svelte
+++ src/components/App.svelte
@@ -1,8 +1,8 @@
 <script>
-  import Menu from "$components/Menu.svelte";
-  import Game from "$components/Game.svelte";
-  import Rules from "$components/Rules.svelte";
-  import About from "$components/About.svelte";
+	import Menu from "$components/Menu.svelte";
+	import Game from "$components/Game.svelte";
+	import Rules from "$components/Rules.svelte";
+	import About from "$components/About.svelte";
 </script>
 
 <Menu />
