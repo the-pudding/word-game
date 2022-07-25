744c226a46853c07e3f9eb9cd86aef702a3178c4
russellgoldenberg <russellgoldenberg@gmail.com>
2022-07-25 09:13:27 -0400

    clock tweaks

--- src/components/Play.Clock.svelte
+++ src/components/Play.Clock.svelte
@@ -7,6 +7,8 @@
 	const target = 60 * MS;
 
 	$: update($active);
+	$: inverse = target - $elapsed;
+	$: width = `${(inverse / target) * 100}%`;
 	$: secondsLeft = Math.ceil((target - $elapsed) / MS);
 	$: if (secondsLeft <= 0) $active = false;
 
@@ -20,32 +22,32 @@
 	};
 </script>
 
-<div class:decreasing={$elapsed > 0} style={`--duration: ${target}ms`} />
+<div>
+	<div class="progress" style:width />
+	<p><strong>{secondsLeft}</strong></p>
+</div>
 
 <style>
 	div {
-		background: orangered;
+		position: relative;
 		width: 100%;
-		height: 30px;
-		transition: width var(--duration) linear;
-		align-self: flex-start;
 	}
-	div.decreasing {
-		width: 0%;
+
+	.progress {
+		position: absolute;
+		top: 0;
+		left: 0;
+		background: var(--color-red-100);
+		width: 100%;
+		height: 2rem;
 	}
+
 	p {
-		font-size: 2em;
-		text-align: center;
-	}
-	@media (min-width: 768px) {
-		p {
-			position: absolute;
-			top: 0;
-			left: 0;
-			font-size: 4em;
-			margin: 0;
-			padding: 0;
-			line-height: 1;
-		}
+		position: relative;
+		line-height: 2rem;
+		margin: 0;
+		padding: 0 8px;
+		font-size: var(--20px);
+		color: var(--color-red-700);
 	}
 </style>
