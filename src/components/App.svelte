e307f081d3f973783c30d2bb9eb140da116326b5
russellgoldenberg <russellgoldenberg@gmail.com>
2022-09-13 10:01:02 -0400

    attempt to fix height

--- src/components/App.svelte
+++ src/components/App.svelte
@@ -1,10 +1,22 @@
 <script>
+	import { onMount } from "svelte";
 	import Game from "$components/Game.svelte";
 	import Rules from "$components/Rules.svelte";
 	import About from "$components/About.svelte";
+
+	let vh;
+
+	const onResize = () => {
+		vh = `${window.innerHeight * 0.01}px`;
+	};
+
+	onMount(() => {
+		window.addEventListener("resize", onResize);
+		onResize();
+	});
 </script>
 
-<article>
+<article style="--vh={vh};">
 	<Game />
 	<Rules />
 	<About />
