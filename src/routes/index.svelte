58acb1d580c4b803d3d04cf72e8733e359e6dfce
russellgoldenberg <russellgoldenberg@gmail.com>
2022-09-15 14:19:55 -0400

    localy test games possible

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,15 +1,22 @@
 <script>
 	import { setContext } from "svelte";
-	import { browser } from "$app/env";
+	import { browser, dev } from "$app/env";
+	import { page } from "$app/stores";
 	import Meta from "$components/Meta.svelte";
 	import App from "$components/App.svelte";
 	import version from "$utils/version.js";
 	import copy from "$data/copy.json";
+	import { wodId } from "$stores/misc.js";
 
 	version();
 
 	const { title, description, url, keywords } = copy;
 	setContext("copy", copy);
+
+	if (browser && dev) {
+		$wodId = $page.url.search.split("id=")[1];
+		console.log($wodId);
+	}
 </script>
 
 <Meta {title} {description} {url} {keywords} />
