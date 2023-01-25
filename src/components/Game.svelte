68b1da1222129a89e4971e57bfd4c62491da5aef
Michelle McGhee <michmac202@gmail.com>
2023-01-24 16:06:12 -0800

    live tournament banner

--- src/components/Game.svelte
+++ src/components/Game.svelte
@@ -73,7 +73,7 @@
 		window.history.replaceState({}, "", `${window.location.pathname}`);
 
 		$allGames = await getAllGames();
-		console.log($allGames);
+		// console.log($allGames);
 
 		$ended = checkEnded();
 
--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,9 +1,33 @@
 <script>
 	import "$styles/app.css";
 	import Header from "$components/Header.svelte";
+	import { gameState } from "$stores/misc.js";
+
+	$: showBanner = $gameState === "pre";
 </script>
 
 <main id="content">
+	{#if showBanner}
+		<a href="https://youtu.be/LO72Mc4yS00" target="_blank"
+			>🎥 watch the tournament of champions live 🎥</a
+		>
+	{/if}
+
 	<Header />
 	<slot />
 </main>
+
+<style>
+	a {
+		height: 2em;
+		background: var(--color-mark-bg);
+		color: var(--color-mark-fg);
+		border: none;
+		margin-bottom: -4px;
+		margin-left: 4px;
+		margin-right: 4px;
+		display: flex;
+		align-items: center;
+		justify-content: center;
+	}
+</style>
